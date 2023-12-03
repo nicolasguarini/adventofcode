e2557f4adc833e1ea4c46d91c63a46cc7f4052fd
Nicolas Guarini <nikiguarincia@gmail.com>
2023-12-04 00:43:04 +0100

    Day 3 - Part 2

--- Day 3: Gear Ratios/script.py
+++ Day 3: Gear Ratios/script.py
@@ -67,9 +67,85 @@ def part_1():
 
     print(f"Day 3 (Part 1): {total_sum}")
 
+def check_line(direction, lines, i, j, h=False):
+    current_number = ''
+
+    if not(i >= 0 and i < len(lines)):
+        return current_number
+    
+    line = lines[i]
+    
+    if h and not line[j].isdigit():
+        return current_number
+
+    if i >= 0 and i < len(lines) and j >= 0 and j < len(line):
+        if line[j].isdigit():
+            current_number += line[j]
+        
+        if j > 0 and j < len(line):
+            j += direction
+
+            while j >= 0 and j < len(line) and line[j].isdigit():
+                current_number += line[j]
+                j += direction
+
+    if direction == -1:
+        current_number = current_number[::-1]
+
+    return current_number
+
+def get_gear_ratio(lines, i, j):
+    nums = []
+    
+    usx = check_line(-1, lines, i-1, j)
+    udx = check_line(1, lines, i-1, j)
+    dx = check_line(1, lines, i, j+1, h=True)
+    sx = check_line(-1, lines, i, j-1, h=True)
+    bsx = check_line(-1, lines, i+1, j)
+    bdx = check_line(1, lines, i+1, j)
+
+    if i > 0:
+        if not lines[i-1][j].isdigit():
+            if usx:
+                nums.append(int(usx))
+            
+            if udx:
+                nums.append(int(udx))
+        else:
+            nums.append(int(usx+udx[1:]))
+    
+    if i < len(lines)-1:
+        if not lines[i+1][j].isdigit():
+            if bsx:
+                nums.append(int(bsx))
+            
+            if bdx:
+                nums.append(int(bdx))
+        else:
+            nums.append(int(bsx+bdx[1:]))
+ 
+    if dx:
+        nums.append(int(dx))
+    
+    if sx: 
+        nums.append(int(sx))
+
+    if len(nums) == 2:
+        return nums[0] * nums[1]
+
+    return 0
+
 def part_2():
-    #TODO
-    pass
+    with open("input.txt") as my_file:
+        lines = my_file.read().split('\n')
+
+    total_sum = 0
+    for i, line in enumerate(lines):
+        for j, character in enumerate(line):
+            if character == '*':
+                total_sum += get_gear_ratio(lines, i, j)
+    
+    print(f"Day 3 (Part 2): {total_sum}")
 
 if __name__ == "__main__":
     part_1()

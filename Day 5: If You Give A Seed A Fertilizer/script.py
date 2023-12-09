578f8502e5f414ff8563d3e4f15756be7a6cc438
Nicolas Guarini <nikiguarincia@gmail.com>
2023-12-09 23:59:32 +0100

    Day 5 - Part 2

--- Day 5: If You Give A Seed A Fertilizer/script.py
+++ Day 5: If You Give A Seed A Fertilizer/script.py
@@ -18,6 +18,40 @@ def part_1():
     seeds = [int(s) for s in lines[0][0].split(': ')[1].split()]
     maps_lines = lines[1:]
 
+    maps = []
+    for line in maps_lines:
+        numbers = line[1:]
+        current_nums = []
+
+        for n in numbers:
+            current_nums.append([int(x) for x in n.split()])
+
+        maps.append(current_nums)
+
+    for j in range(len(seeds)):
+        for map in maps:
+            for dest_start, src_start, ran_len in map:
+                if seeds[j] >= src_start and seeds[j] <= src_start + ran_len - 1:
+                    if dest_start < src_start:
+                        seeds[j] -= src_start - dest_start
+                        break
+                    else:
+                        seeds[j] += dest_start - src_start
+                        break
+
+    print(f"Day 5 (Part 1): {min(seeds)}")
+
+def get_seeds(seeds):
+    return [j for i in range(0, len(seeds), 2) for j in range(seeds[i], seeds[i] + seeds[i + 1])]
+
+def part_2():
+    with open("input.txt") as my_file:
+        lines = my_file.read().splitlines()
+    
+    lines = split_list(lines)
+    
+    seeds = [int(s) for s in lines[0][0].split(': ')[1].split()]
+    maps_lines = lines[1:]
 
     maps = []
     for line in maps_lines:
@@ -29,22 +63,21 @@ def part_1():
 
         maps.append(current_nums)
 
-    print(f"Seeds: {str(seeds)}")
-    for i, map in enumerate(maps, start=1):
-        d = {}
-        for dest_start, src_start, ran_len in map:
-            range_src = range(src_start, src_start + ran_len)
-            range_dest = range(dest_start, dest_start + ran_len)
+    seeds = get_seeds(seeds)
 
-            d.update(zip(range_src, range_dest))
+    for j in range(len(seeds)):
+        for map in maps:
+            for dest_start, src_start, ran_len in map:
+                if seeds[j] >= src_start and seeds[j] <= src_start + ran_len - 1:
+                    if dest_start < src_start:
+                        seeds[j] -= src_start - dest_start
+                        break
+                    else:
+                        seeds[j] += dest_start - src_start
+                        break
 
-        for j, n in enumerate(seeds):
-            if n in d:
-                seeds[j] = d[n]
-        
-        print(f"Seeds (map {i}): {str(seeds)}")
-
-    print(f"Day 5 (Part 1): {min(seeds)}")
+    print(f"Day 5 (Part 2): {min(seeds)}")
 
 if __name__ == "__main__":
     part_1()
+    part_2()
